3e4a7848e6000c43e4a14e335b7d1f4f8d38926f
Jefferson Simão Gonçalves <gerson.simao.92@gmail.com>
2022-04-16 10:19:08 -0300

    Adicionando a animação no react native

--- App.js
+++ App.js
@@ -1,20 +1,34 @@
+import React from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, TouchableOpacity, View } from 'react-native';
+import Feather from "react-native-vector-icons/Feather";
+import LottieView from 'lottie-react-native';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    let animation = React.createRef();
+
+    function startAnimation() {
+        animation.current.play();
+    }
+
+    return (
+        <View style={ styles.container }>
+            <TouchableOpacity onPress={ startAnimation }>
+                <Feather name="play" size={ 60 } color="red" />
+            </TouchableOpacity>
+            <LottieView source={ require('./assets/check.json') } loop={ false }
+                        style={ { width: 200, height: 200 } } ref={ animation } />
+            <Button title="Start Animação" onPress={ startAnimation } />
+            <StatusBar style="auto" />
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
 });
